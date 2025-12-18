a3b68995de137f9f4c267054b7736e1bd037250c
Thoai1503 <vothoai1503@gmail.com>
2025-12-18 09:30:00 +0700

    update 3

--- backend/src/main/java/tinhvomon/com/db/ConnectDataSource.java
+++ backend/src/main/java/tinhvomon/com/db/ConnectDataSource.java
@@ -21,17 +21,24 @@ public class ConnectDataSource {
         // ds.setTrustServerCertificate(true); // 
          ds = new SQLServerDataSource();
          
-         String dbUser = System.getenv("DB_USER");
-         String dbPass = System.getenv("DB_PASSWORD");
-         String dbServer = System.getenv("DB_SERVER");
-         String dbName = System.getenv("DB_NAME");
-         String dbPort = System.getenv("DB_PORT");
+        //  String dbUser = System.getenv("DB_USER");
+        //  String dbPass = System.getenv("DB_PASSWORD");
+        //  String dbServer = System.getenv("DB_SERVER");
+        //  String dbName = System.getenv("DB_NAME");
+        //  String dbPort = System.getenv("DB_PORT");
          
-         ds.setUser(dbUser != null ? dbUser : "sa"); 
-         ds.setPassword(dbPass); // Mật khẩu bí mật lấy từ Docker
-         ds.setServerName(dbServer != null ? dbServer : "localhost");
-         ds.setPortNumber(dbPort != null ? Integer.parseInt(dbPort) : 1433);
-         ds.setDatabaseName(dbName != null ? dbName : "ProductDB");
+        //  ds.setUser(dbUser != null ? dbUser : "sa"); 
+        //  ds.setPassword(dbPass); // Mật khẩu bí mật lấy từ Docker
+        //  ds.setServerName(dbServer != null ? dbServer : "localhost");
+        //  ds.setPortNumber(dbPort != null ? Integer.parseInt(dbPort) : 1433);
+        //  ds.setDatabaseName(dbName != null ? dbName : "ProductDB");
+
+          ds.setUser("sa");
+        ds.setPassword("Thoaivip@13");
+        ds.setServerName("103.90.225.130");
+        ds.setPortNumber(1433);
+        ds.setDatabaseName("ProductDB");       
+        ds.setTrustServerCertificate(true); // 
          
 
     }
