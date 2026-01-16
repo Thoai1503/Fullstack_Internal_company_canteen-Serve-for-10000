05893a08800e5c7d63fd2364405912ef7d704425
Thoai1503 <vothoai1503@gmail.com>
2026-01-16 09:39:02 +0700

    update

--- backend/src/main/java/tinhvomon/com/db/ConnectDataSource.java
+++ backend/src/main/java/tinhvomon/com/db/ConnectDataSource.java
@@ -1,17 +1,47 @@
 package tinhvomon.com.db;
 
+import java.io.InputStream;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
-public class ConnectDataSource {
+public final class ConnectDataSource {
 
 	private static SQLServerDataSource ds;
+	   private static final Properties props = new Properties();
+	   
+	   
+	   private static String getEnvOrProp(String envKey, String propKey) {
+	        String env = System.getenv(envKey);
+	        if (env != null && !env.isBlank()) {
+	            return env;
+	        }
+	        return props.getProperty(propKey);
+	    }
+	   
+	   
+	 // Load config 1 lần duy nhất
+    static {
+        try (InputStream input = ConnectDataSource.class
+                .getClassLoader()
+                .getResourceAsStream("application.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("❌ Không tìm thấy application.properties");
+            }
+            System.out.print("Find application.properties");
+            props.load(input);
+        } catch (Exception e) {
+            throw new RuntimeException("❌ Lỗi load DB config", e);
+        }
+    }
 
     static {
+    	  String port = getEnvOrProp("DB_PORT", "sqlserver.port");
         // ds = new SQLServerDataSource();
         // ds.setUser("sa");
         // ds.setPassword("123");
@@ -33,11 +63,21 @@ public class ConnectDataSource {
         //  ds.setPortNumber(dbPort != null ? Integer.parseInt(dbPort) : 1433);
         //  ds.setDatabaseName(dbName != null ? dbName : "ProductDB");
 
-          ds.setUser("sa");
-        ds.setPassword("Thoaivip@13");
-        ds.setServerName("103.90.225.130");
-        ds.setPortNumber(1433);
-        ds.setDatabaseName("ProductDB");       
+//          ds.setUser("sa");
+//        ds.setPassword("thoaivip@13");
+//        ds.setServerName("103.90.225.130");
+//        ds.setPortNumber(1433);
+//        ds.setDatabaseName("ProductDB");       
+//        ds.setEncrypt(false);              
+//        ds.setTrustServerCertificate(true); 
+//        ds.setLoginTimeout(10); 
+//        
+        
+        ds.setUser(getEnvOrProp("DB_USER", "sqlserver.username"));
+        ds.setPassword(getEnvOrProp("DB_PASSWORD", "sqlserver.password"));
+        ds.setServerName(getEnvOrProp("DB_HOST", "sqlserver.host"));
+        ds.setPortNumber(Integer.parseInt(port));
+        ds.setDatabaseName(getEnvOrProp("DB_NAME", "sqlserver.database"));       
         ds.setEncrypt(false);              
         ds.setTrustServerCertificate(true); 
         ds.setLoginTimeout(10); 
